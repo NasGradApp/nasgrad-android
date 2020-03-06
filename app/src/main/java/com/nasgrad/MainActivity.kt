41eee9149077e6e743947b8cd9010d07edf75cd6
Sonja Mijatovic <sonja.mijatovic@gmail.com>
2020-03-06 12:23:03 +0100

    Implementing Report an issue button on the main screen

--- app/src/main/java/com/nasgrad/MainActivity.kt
+++ app/src/main/java/com/nasgrad/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.nasgrad
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.nasgrad.adapter.IssueAdapter
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
 import com.nasgrad.adapter.OnItemClickListener
-import com.nasgrad.api.model.Issue
 import com.nasgrad.issue.CreateIssueActivity
 import com.nasgrad.nasGradApp.R
 import com.nasgrad.utils.Helper
 import com.nasgrad.utils.SharedPreferencesHelper
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
+
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, OnItemClickListener {
 
@@ -30,27 +32,61 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, OnItemClickListene
 
     private var disposable: Disposable? = null
     private var map: GoogleMap? = null
+    private var marker: MarkerOptions? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+
         Timber.plant(Timber.DebugTree())
 
-        fab.setOnClickListener {
+        reportIssue.setOnClickListener {
             startActivity(Intent(this@MainActivity, CreateIssueActivity::class.java))
         }
         createUserId()
 //        setupAdapter()
         showIssues()
+        marker =
+            MarkerOptions().icon(bitmapDescriptorFromVector(R.drawable.ic_location_pin))
+
+    }
+
+    private fun bitmapDescriptorFromVector(vectorResId: Int): BitmapDescriptor {
+        val vectorDrawable = ContextCompat.getDrawable(this, vectorResId)
+        vectorDrawable?.setBounds(
+            0,
+            0,
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight
+        )
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable?.intrinsicWidth ?: 0, vectorDrawable?.intrinsicHeight
+                ?: 0, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        vectorDrawable?.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
         Timber.d("onMapReady")
         this.map = googleMap
+        val noviSad = LatLng(45.246216, 19.801637)
+        marker?.position(noviSad)
+        googleMap?.addMarker(marker)
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(noviSad))
     }
 
-    override fun onItemClicked(itemId: String, itemTitle: String?, itemType: String?, itemDesc: String?, itemImage: String?) {
+    override fun onItemClicked(
+        itemId: String,
+        itemTitle: String?,
+        itemType: String?,
+        itemDesc: String?,
+        itemImage: String?
+    ) {
 
         val detailsActivityIntent: Intent = Intent(this, DetailActivity::class.java).apply {
             putExtra(ITEM_ID, itemId)
